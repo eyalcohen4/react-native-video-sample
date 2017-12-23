2f24d09f88c9bbb96bf4b375f8af7c985986d293
Eyal Cohen <eyal@myplay.com>
2017-12-23 16:20:04 +0200

    react native video sample

---
+++ App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Video } from 'expo';
+import { MaterialIcons, Octicons } from '@expo/vector-icons';
+
+export default class App extends React.Component {
+	state = {
+		mute: false,
+		fullScreen: false,
+		shouldPlay: true,
+	}
+
+	handlePlayAndPause = () => {
+		this.setState(prevState => ({
+			shouldPlay: !prevState.shouldPlay
+		}));
+	}
+
+	handleVolume = () => {
+		this.setState(prevState => ({
+			mute: !prevState.mute,
+		}));
+	}
+
+  render() {
+		const { width } = Dimensions.get('window');
+		
+    return (
+      <View style={styles.container}>
+				<View>
+						<Text style={{ textAlign: 'center' }}> React Native Video </Text>
+						<Video
+							source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+							shouldPlay={this.state.shouldPlay}
+							resizeMode="cover"
+							style={{ width, height: 300 }}
+							isMuted={this.state.mute}
+						/>
+						<View style={styles.controlBar}>
+							<MaterialIcons 
+								name={this.state.mute ? "volume-mute" : "volume-up"}
+								size={45} 
+								color="white" 
+								onPress={this.handleVolume} 
+							/>
+							<MaterialIcons 
+								name={this.state.shouldPlay ? "pause" : "play-arrow"} 
+								size={45} 
+								color="white" 
+								onPress={this.handlePlayAndPause} 
+							/>
+						</View>
+					</View>
+      </View>
+		);
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+	},
+	controlBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+		height: 45,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+	}
+});
